15de474c6bc15aab71842dc76c49e20d934f992c
Nikita Ilyanov <nikita.glk@mail.ru>
2022-04-28 19:44:24 +0300

    feat: complete count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(backyard) {
+  
+  let count = 0;
+  if(!backyard || !Array.isArray(backyard)) {
+    return false;
+  }
+  backyard.flat().forEach(element => {
+    if (element === '^^'){
+      count++;
+    }
+  });
+  return count;
 }
 
 module.exports = {
