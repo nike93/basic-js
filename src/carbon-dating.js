a27b0a4e7a10bdba139953ea896069ea557a063f
Nikita Ilyanov <nikita.glk@mail.ru>
2022-04-29 18:41:35 +0300

    feat: complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+ 
+  if(typeof sampleActivity !== 'string' || typeof(Number(sampleActivity)) !== 'number' || Number(sampleActivity) < 1 || Number(sampleActivity) > MODERN_ACTIVITY || isNaN(parseInt(sampleActivity))) {
+//console.log('false');
+    return false;
+  } else {
+    
+    //console.log(k);
+    let t = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.0693 / HALF_LIFE_PERIOD) * 0.1); 
+    //console.log(t);
+    return t;
+  }
+    
+  
 }
 
 module.exports = {
