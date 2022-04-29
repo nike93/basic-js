b0c27c37aad757da2be7eb9493525d85c2d60252
Nikita Ilyanov <nikita.glk@mail.ru>
2022-04-29 19:31:36 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  } else {
+    let filterArr = members.filter(elem => typeof elem === 'string');
+    let teamName = [];
+    filterArr.forEach(item => {
+      teamName.push(item.replace(/\s/g, '').charAt(0).toUpperCase());
+    })
+    teamName = teamName.sort().join('');
+    return teamName;
+  }
 }
 
 module.exports = {
