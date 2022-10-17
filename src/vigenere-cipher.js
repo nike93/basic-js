128c89dcbcb63abc8d4acb182d7a97e237e8bcee
nike93 <78583165+nike93@users.noreply.github.com>
2022-10-17 10:45:19 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  decoder(n1, n2, message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let counter = 0;
+    let messageUpper = message.toUpperCase().split('');
+    let keyUpper = key.toUpperCase().split('').map(char => char.charCodeAt(0) - 65);
+    let result = [];
+    for (let i = 0; i < messageUpper.length; i++) {
+      let position = messageUpper[i].charCodeAt(0);
+
+      if (position >= 65 && position < 91) {
+        result.push(String.fromCharCode(65 + (position - 65 + n1 * keyUpper[counter % keyUpper.length] + n2) % 26));
+        counter++;
+      } else {
+        result.push(messageUpper[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
+  }
+
+  encrypt(message, key) {
+    let n1 = 1;
+    let n2 = 0;
+    return this.decoder(n1, n2, message, key);
+  }
+
+  decrypt(message, key) {
+    let n1 = -1;
+    let n2 = 26;
+    return this.decoder(n1, n2, message, key);
   }
 }
 
